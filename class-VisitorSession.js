71327d56cbf45b55c873a989d64ffdc80688ec27
Eli Hart <elihart@stanford.edu>
2013-08-08 17:19:09 -0700

    Support for entering visitor name and email on the visitor side

--- class-VisitorSession.js
+++ class-VisitorSession.js
@@ -2,51 +2,135 @@
  VisitorSession manages a visitor's interaction with the Maqaw client. It contains the connection
  with a representative, and handles all display and transfer of communication with that rep.
  */
-function MaqawVisitorSession(manager) {
+function MaqawVisitorSession(manager, visitorInfo) {
     var that = this;
     this.chatSession;
     this.maqawManager = manager;
+
+    // Visitor info was passed in if it was previously stored. Otherwise it is undefined
+    this.visitorInfo = null;
+    if(visitorInfo){
+        this.visitorInfo = new MaqawVisitorInfo(visitorInfo);
+    }
 
     // the status of our connection with a peer. True for open and false for closed
     // Defaults to false until we can verify that a connection has been opened
     this.isConnected = false;
 
-    // initialize header container for this session
+    /* initialize header container for this session */
     this.header = document.createElement('DIV');
-    this.header.className = 'maqaw-default-client-header';
+    this.header.className = 'maqaw-visitor-session-header';
+    // div to hold text in header
+    this.headerText = document.createElement('DIV');
+    this.headerText.className = 'maqaw-visitor-session-header-text';
+    this.header.appendChild(this.headerText);
+    // function to change the header text
+    function changeHeaderText(text){
+        that.headerText.innerHTML = text;
+    }
+    // set default text
+    changeHeaderText("Chat with us!");
 
-    // initialize body container
+    /* initialize body container */
     this.body = document.createElement('DIV');
+    this.body.className = 'maqaw-visitor-session-body';
+    // content div holds the main content in the body
+    this.bodyContent = document.createElement('DIV');
+    this.body.appendChild(this.bodyContent);
+    // function to set what content is shown
+    function setBodyContent(div){
+        that.bodyContent.innerHTML = '';
+        that.bodyContent.appendChild(div);
+    }
 
-    /* Create elements that make up chat window */
-    this.loginHeader = document.createElement('DIV');
-    this.loginHeader.innerHTML = "Chat with me!";
-    this.loginHeader.className = 'maqaw-chat-header-text';
-
-    // create div to hold chat info
-    this.visitorChatWindow = document.createElement('DIV');
-    this.visitorChatWindow.className = 'maqaw-client-chat-window';
-
-    // add chat session
-    var chatSessionContainer = document.createElement("DIV");
-    this.visitorChatWindow.appendChild(chatSessionContainer);
+    /* Create chat container and session */
+     var chatSessionContainer = document.createElement("DIV");
     this.chatSession = new MaqawChatSession(chatSessionContainer, sendTextFromChat, 'You', this.maqawManager.chatName);
 
-    // set up a connection listener to wait for a rep to make a connection with us
-    this.connection;
+    /* Create container for when no rep is available */
+    var noRepContainer = document.createElement("DIV");
+    noRepContainer.id = 'maqaw-no-rep-window';
+    noRepContainer.innerHTML = 'Sorry, there are no representatives available to chat';
+    // default to showing the noRepContainer until a connection with a rep is made
+    setBodyContent(noRepContainer);
 
+    /* Create a form to get the visitor's name and email address before chatting */
+    var visitorInfoContainer = document.createElement("DIV");
+    visitorInfoContainer.id = 'maqaw-visitor-session-info';
+    // Text instructions
+    var infoInstructions = document.createElement("DIV");
+    infoInstructions.id = 'maqaw-visitor-info-instructions';
+    infoInstructions.innerHTML = "Enter your name and email to start chatting with us!";
+    visitorInfoContainer.appendChild(infoInstructions);
+    // field for visitor name
+    var nameField = document.createElement("input");
+    nameField.setAttribute('type', "text");
+    nameField.setAttribute('id', "maqaw-visitor-name-field");
+    nameField.setAttribute('placeholder', 'Name');
+    visitorInfoContainer.appendChild(nameField);
+    // field for visitor email
+    var emailField = document.createElement("input");
+    emailField.setAttribute('type', "text");
+    emailField.setAttribute('id', "maqaw-visitor-email-field");
+    emailField.setAttribute('placeholder', 'Email');
+    visitorInfoContainer.appendChild(emailField);
+    // submit button
+    var infoSubmitButton = document.createElement('DIV');
+    infoSubmitButton.id = 'maqaw-visitor-info-button';
+    infoSubmitButton.innerHTML = 'Ok';
+    visitorInfoContainer.appendChild(infoSubmitButton);
+    // submit button callback
+    infoSubmitButton.addEventListener('click', visitorInfoEntered, false);
+    function visitorInfoEntered(){
+        var name = nameField.value;
+        var email = emailField.value;
+        // TODO: Display error message for invalid name or email
+        // check to make sure name and email aren't blank
+        if(name !== '' && email !== ''){
+            // store the visitor's info
+            that.visitorInfo = new MaqawVisitorInfo({
+                name: name,
+                email: email
+            });
+            // show the chat window
+            setBodyContent(chatSessionContainer);
+        }
+    }
+
+    /* Add footer to body */
+    this.bodyFooter = document.createElement('DIV');
+    this.bodyFooter.id = 'maqaw-visitor-session-footer';
+    this.body.appendChild(this.bodyFooter);
+
+    // add login button to footer
+    var loginButton = document.createElement('DIV');
+    loginButton.id = 'maqaw-login-button';
+    loginButton.innerHTML = "Login";
+    this.bodyFooter.appendChild(loginButton);
+
+    // setup callback for when login is clicked
+    loginButton.addEventListener('click', this.maqawManager.loginClicked, false);
+
+    // add Maqaw link to footer
+    var maqawLink = document.createElement('DIV');
+    maqawLink.id = 'maqaw-link';
+    maqawLink.innerHTML = 'POWERED BY <a href="http://maqaw.com">MAQAW</a>';
+    this.bodyFooter.appendChild(maqawLink);
+
+    /* Set up the connection */
+    this.connection = null;
     this.mirror = new Mirror();
 
-    this.maqawManager.connectionManager.on('connection', function(maqawConnection) {
-      if (that.connection) {
-        console.log("Warning: Overwriting existing connection");
-      }
-      that.connection = maqawConnection;
-      that.mirror.setConnection(that.connection);
+    this.maqawManager.connectionManager.on('connection', function (maqawConnection) {
+        if (that.connection) {
+            console.log("Warning: Overwriting existing connection");
+        }
+        that.connection = maqawConnection;
+        that.mirror.setConnection(that.connection);
 
-      maqawConnection.on('data', connectionDataCallback)
-        .on('change', connectionStatusCallback) 
-    }); 
+        maqawConnection.on('data', connectionDataCallback)
+            .on('change', connectionStatusCallback)
+    });
 
     /*
      * For a connection received from the newConnectionListener, this function will be called by the connection
@@ -58,7 +142,7 @@ function MaqawVisitorSession(manager) {
             that.chatSession.newTextReceived(data.text);
         }
         if (data.type === MAQAW_DATA_TYPE.SCREEN) {
-          that.mirror && that.mirror.data(data);
+            that.mirror && that.mirror.data(data);
         }
     }
 
@@ -68,7 +152,6 @@ function MaqawVisitorSession(manager) {
      * with true representing an open connection and false representing closed.
      */
     function connectionStatusCallback(connectionStatus) {
-        //console.log("Visitor Session connection status: "+connectionStatus);
         that.isConnected = connectionStatus;
 
         // update chat session to reflect connection status
@@ -76,10 +159,17 @@ function MaqawVisitorSession(manager) {
 
         // show a different page if there is no connection with a rep
         if (connectionStatus) {
-            setClientChat();
+            // if they've enter their info, show them the chat window
+            if(that.visitorInfo){
+                setBodyContent(chatSessionContainer);
+            }
+            // otherwise ask for their information
+            else {
+                setBodyContent(visitorInfoContainer);
+            }
         }
         else {
-            setNoRepPage();
+            setBodyContent(noRepContainer);
         }
     }
 
@@ -93,85 +183,6 @@ function MaqawVisitorSession(manager) {
         } else {
             that.connection.sendText(text);
         }
-    }
-
-
-    // add footer
-    var chatFooter;
-    chatFooter = document.createElement('DIV');
-    chatFooter.id = 'maqaw-chat-footer';
-    this.visitorChatWindow.appendChild(chatFooter);
-
-    // add login button to footer
-    var loginButton;
-    loginButton = document.createElement('DIV');
-    loginButton.id = 'maqaw-login-button';
-    loginButton.innerHTML = "Login"
-    chatFooter.appendChild(loginButton);
-
-    // setup callback for when login is clicked
-    loginButton.addEventListener('click', this.maqawManager.loginClicked, false);
-
-    // add Maqaw link to footer
-    var maqawLink;
-    maqawLink = document.createElement('DIV');
-    maqawLink.id = 'maqaw-link';
-    maqawLink.innerHTML = 'POWERED BY <a href="http://maqaw.com">MAQAW</a>';
-    chatFooter.appendChild(maqawLink);
-
-    /* Create container for when no rep is available */
-    this.noRepWindow = document.createElement("DIV");
-    this.noRepWindow.id = 'maqaw-no-rep-window';
-    this.noRepWindow.className = 'maqaw-client-chat-window'
-    this.noRepWindow.innerHTML = '';
-
-    var noRepText = document.createElement("DIV");
-    noRepText.className = 'maqaw-chat-display';
-    noRepText.innerHTML = 'Sorry, there are no representatives available to chat';
-    this.noRepWindow.appendChild(noRepText);
-
-    this.noRepHeader = document.createElement('DIV');
-    this.noRepHeader.innerHTML = "Send us an email!";
-    this.noRepHeader.className = 'maqaw-chat-header-text';
-
-    // add footer
-    var noRepFooter;
-    noRepFooter = document.createElement('DIV');
-    noRepFooter.id = 'maqaw-chat-footer';
-    this.noRepWindow.appendChild(noRepFooter);
-
-    // add login button to footer
-    var noRepLoginButton;
-    noRepLoginButton = document.createElement('DIV');
-    noRepLoginButton.id = 'maqaw-login-button';
-    noRepLoginButton.innerHTML = "Login"
-    noRepFooter.appendChild(noRepLoginButton);
-
-    // setup callback for when login is clicked
-    noRepLoginButton.addEventListener('click', this.maqawManager.loginClicked, false);
-
-    // add Maqaw link to footer
-    var maqawLink;
-    maqawLink = document.createElement('DIV');
-    maqawLink.id = 'maqaw-link';
-    maqawLink.innerHTML = 'POWERED BY <a href="http://maqaw.com">MAQAW</a>';
-    noRepFooter.appendChild(maqawLink);
-
-    // set the chat window to default to no rep
-    setNoRepPage();
-
-    function setClientChat() {
-        that.body.innerHTML = '';
-        that.body.appendChild(that.visitorChatWindow);
-        that.header.innerHTML = '';
-        that.header.appendChild(that.loginHeader);
-    }
-
-    function setNoRepPage() {
-        that.body.innerHTML = '';
-        that.body.appendChild(that.noRepWindow);
-        that.header.innerHTML = '';
-        that.header.appendChild(that.noRepHeader);
     }
 
     // returns an object containing the data that constitutes this visitors session
@@ -195,3 +206,11 @@ MaqawVisitorSession.prototype.getBodyContents = function () {
 MaqawVisitorSession.prototype.getHeaderContents = function () {
     return this.header;
 };
+
+/*
+ * Store information about this visitor
+ */
+function MaqawVisitorInfo (info) {
+    this.name = info.name;
+    this.email = info.email;
+}
